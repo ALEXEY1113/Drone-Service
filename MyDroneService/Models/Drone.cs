0a4b88c8c18b9ed036f4be89b14170bf4d9da594
Ronald P.A <stukov13@hotmail.com>
2023-03-30 03:16:21 -0400

    Initial Commit

---
+++ MyDroneService/Models/Drone.cs
@@ -0,0 +1,14 @@
+﻿namespace MyDroneService.Models
+{
+    public class Drone
+    {
+        public string Name { get; set; }
+        
+        public int MaxLoadWeight { get; set; }
+
+        public Drone(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ MyDroneService/Models/LocationDelivery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDroneService.Models
+{
+    public class LocationDelivery
+    {
+        public string Name { get; set; }
+
+        public int PackageWeight { get; set; }
+
+        public LocationDelivery(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
